036412dcf137b4724854589e4fd0a4c0919af9d8
vishal <vishal.rakshe@algorisys.com>
2020-07-24 21:18:13 +0530

    Updates

---
+++ precache-manifest.f29e9cefdac7dba29e0cf0f75ddaf68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc0847ee3a8bc38a2cd581c1fc1fb12",
+    "url": "/sokra-web/index.html"
+  },
+  {
+    "revision": "78c6de0afb587bb349fa",
+    "url": "/sokra-web/static/css/main.2e092192.chunk.css"
+  },
+  {
+    "revision": "075d50f8dabdc88af8da",
+    "url": "/sokra-web/static/js/2.929f8f9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sokra-web/static/js/2.929f8f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c6de0afb587bb349fa",
+    "url": "/sokra-web/static/js/main.8fd76663.chunk.js"
+  },
+  {
+    "revision": "cee21ae088eaf08f2673",
+    "url": "/sokra-web/static/js/runtime-main.3e6c0108.js"
+  }
+]);
