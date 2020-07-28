636cfa7fd9d94a7cf999f6a70fdea378ec16c9c5
vishal <vishal.rakshe@algorisys.com>
2020-07-28 10:59:47 +0530

    terms and condition modal added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,87 @@ function Footer() {
                </div>
            </div>
            <hr />
-           <p className="terms"> <a href="#">Terms and Conditions</a></p>
+           <p className="terms"> <a href="#" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Terms and Conditions</a></p>
+
+     
+            <div className="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-lg" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Terms and Conditions</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                    <p className="text-uppercase text-center">Privacy Policy</p>
+                    <p className="font-weight-light">
+                        SOKRA Academy feels strongly about protecting your privacy. We understand how important privacy is to you, and we are committed to creating a safe and secure environment for learners of all ages.In order to honour your trust, SOKRA Academy follows the practice of ethical standards in collecting, using, and safeguarding any information you make available for use.
+                        This privacy policy governs your use of the application ‘SOKRA Academy’
+                        (“Application”).
+                        We strive to provide you with access to and control over the information you give us, and we take the protection of your information very seriously.
+                        
+                        Your use of the Application signifies your agreement to be legally bound by the
+                        same and your acceptance of this Policy.
+                        Before using the Application please make sure you read this privacy policy (“Policy“) carefully. Kindly contact us at support@sokra.academy if you wish not to agree with any of the terms and conditions of this Policy before using the
+                        application further.
+                        
+                        User Provided Information :
+                        
+                        Our primary goal in collecting your Personal Data and other personal information is to provide you a safe, efficient, smooth and customized experience. We collect your Personal Data when you register with us, when you express an interest in obtaining information about us or our products and services, when you participate in activities on our app. The Application records the information When you install it for the first time. When you register with us, you generally provide (a) your name, email address, language preference, board of education and standard (If you are student); 
+                        
+                        SOKRA Academy may use this Information to contact you from time to time, to provide you with the Services, important information, required notices and marketing promotions. SOKRA Academy will request you when it needs more information that personally identifies you (personal information) or allows us to contact you.
+                        
+                        
+                        
+                        Use of your Personal Information :
+                        
+                        We use the personal information we collect, including your Personal Data where it is necessary to deliver the services you have requested. SOKRA Academy uses the collected Information to examine methodically and in detail, trends, to carry out research, to administer the Application/Services and products, to learn about each user’s learning patterns and movements around the Application/Services and products and to gather demographic information and usage behaviour about its user base as a whole. Aggregated and individual, anonymized and non-anonymized data may periodically be transmitted to external service providers to help us improve the Application, products and its Services. SOKRA Academy will never share your information
+                        with third parties without your permission.
+                        
+                        
+                        
+                        Automatically Collected Information :
+                        
+                        In addition, the Application may gather certain information automatically,
+                        including, but not limited to, your mobile devices unique device ID, the type
+                        of mobile devices you use.
+                        To better your learning objectives/experience SOKRA Academy may use the individual data and behavior patterns combined with personal information to provide you with personalized content.
+                        Sometime SOKRA Academy will send email notices or contact you to communicate about its Services, products and benefits, as they are considered an essential part of the Services/products you have chosen.
+                        
+                        
+                        
+                        SOKRA Academy may disclose Information:
+                        We use other companies, agents or contractors ("Service Providers") to perform services on our behalf or to assist us with the provision of services to you. We do not authorize them to use or disclose your personal information except in connection with providing their services.
+                        When it believes in good faith (doctrine of uberrima fides) that the disclosure is necessary to protect its rights, protect your safety or the safety of others, investigate fraud, address security or technical issues or respond to a government request;
+                        As required by law, such as to comply with a subpoena, or similar legal process;
+                        To enforce applicable ToU, including investigation of potential violations thereof;
+                        
+                        With its trusted services providers who have agreed to and adhered to the rules set forth in this Policy and work on its behalf, do not have an independent use of the information SOKRA Academy discloses to them.
+
+                        Security :
+                        We view protection of your privacy as a very important community principle. We understand clearly that you and your Information is one of our most important assets. SOKRA Academy provide physical, electronic, and procedural safeguards to protect Information it processes and maintain as SOKRA Academy is concerned about safeguarding the confidentiality of your Information. Under no circumstances, we rent, trade, transfer or share your personal information that we have collected with any other company for their marketing purposes without your consent.
+                        An illustration being, SOKRA Academy limits access to this Information to authorized employees only who need to know that information in order to operate, develop or improve its Application/Services/products. Please be aware that, although SOKRA Academy endeavour is to provide stringent security, for information it possesses and maintains, no security system can prevent all potential security breaches.
+                        
+                        How long does SOKRA Academy retain user data?
+                        You can explicitly request us to remove data from our database by sending mail to support@sokra.academy. We keep data in our database which contains your name, email address and learning preferences. 
+
+                        Changes to this Statement :
+                        
+                        As the Company (SOKRAAcademy) evolves, its privacy policy will need to evolve as well to cover new situations. We’ll review this Privacy Policy from time to time to make sure it is up-to-date. You are advised to review this Policy regularly for any changes, as continued use is deemed approval of all changes.
+                        
+                        Your Consent :
+                        By registering with it, you are expressly consenting to its collection, processing, storing, disclosing and handling of your information as set forth in this Policy now and as amended by us. SOKRA Academy believes that, every user of our Application must be in a position to provide an informed consent prior to providing any Information required for the use of the Application. Processing, your information in any way, including, but not limited to, collecting, storing, deleting, using, combining, sharing, transferring and disclosing information, all of which activities will take place in India. If you reside outside India your information will be transferred, processed and stored in accordance with the applicable data protection laws of India.
+                        In case of any queries, reach out to us on support@sokra.academy.
+                        </p>
+                    </div>
+                    <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                </div>
+                </div>
+            </div>
+       </div>
        </div>
    </section>
   );
